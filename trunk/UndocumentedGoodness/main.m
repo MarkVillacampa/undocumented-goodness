9a79ea60ac225eb4a0839f9fed1fb91aa59edc13
jnj <jnj@8fbc7e05-9e47-0410-871a-19883e41bc2c>
2008-03-19 15:51:29 +0000

    Added test project, c++ bracketing

---
+++ trunk/UndocumentedGoodness/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UndocumentedGoodness
+//
+//  Created by Nicholas Jitkoff on 3/19/08.
+//  Copyright Google Inc 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "QLUIPrivate.h"
+#import "CPSPrivate.h"
+#import "CGSPrivate.h"
+#import "CoreDockPrivate.h"
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
